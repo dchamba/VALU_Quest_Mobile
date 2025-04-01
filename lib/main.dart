c1236c3f0141160cec00cf31ea4e3036a32da345
dchamba <dchamba91@gmail.com>
2025-04-01 17:59:57 +0200

    Pagina risultati finale mostrata secondo Config
    Pagina risultati finale mostrata secondo Config e inserito messaggio finale Grazie-Completamento  con check verde

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:valu_quest/TryCode.dart';
 import 'package:valu_quest/test.dart';
 import 'package:valu_quest/view/bmi/bmi_calculator.dart';
 import 'package:valu_quest/view/splash/splash_screen.dart';
 
 void main() {
+  //new TestTryCode();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false,
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Valu Quest',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
@@ -22,4 +24,6 @@ class MyApp extends StatelessWidget {
       home: const SplashScreen(),
     );
   }
+
+
 }
